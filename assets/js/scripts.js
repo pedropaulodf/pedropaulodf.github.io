7e462d52b2b4c02cb2040594bbde0b9567175867
Pedro Paulo <pedropaulotj@gmail.com>
2021-12-09 08:08:53 -0300

    ajuste botão discord

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,10 @@
 const spanMessageCopiadoEmail = document.querySelector(".email");
-const spanMessageCopiadoDiscord = document.querySelector(".discord");
 
-function copiarTexto(value, local) {
+function copiarTexto(value) {
   const durationVisible = 1500;
   navigator.clipboard.writeText(value);
-  if (local === 1) {
-    spanMessageCopiadoEmail.style.opacity = 1;
-    setTimeout(() => {
-      spanMessageCopiadoEmail.style.opacity = 0;
-    }, durationVisible);
-  } else if (local === 2) {
-    spanMessageCopiadoDiscord.style.opacity = 1;
-    setTimeout(() => {
-      spanMessageCopiadoDiscord.style.opacity = 0;
-    }, durationVisible);
-  }
+  spanMessageCopiadoEmail.style.opacity = 1;
+  setTimeout(() => {
+    spanMessageCopiadoEmail.style.opacity = 0;
+  }, durationVisible);
 }
